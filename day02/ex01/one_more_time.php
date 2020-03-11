8db37da6538faf5862af783f8a84463649ca2fec
Antti Vornanen <anttivornanen@Antti-iMac.home>
2020-03-11 22:05:38 +0200

    Added ex01

--- day02/ex01/one_more_time.php
+++ day02/ex01/one_more_time.php
@@ -1,15 +1,12 @@
 #!/usr/bin/php
 <?php
-	setlocale(LC_TIME, "fr_FR.UTF8");
+	setlocale(LC_TIME, "fr_FR");
 	if ($argc != 2)
 		return;
 	$str = explode(' ', $argv[1]);
 	if (count($str) != 5)
-	echo "Wrong Format\n";
+		echo "Wrong Format\n";
 	$time = explode(":", $str[4]);
-	print_r($str);
-	print_r($time);
-	
 	$months = array("Janvier" => 1,
 					"Février" => 2,
 	 				"Mars" => 3,
@@ -22,6 +19,27 @@
 	 				"Octobre" => 10,
 	 				"Novembre" => 11,
 					"Décembre" => 12);
-	echo strftime("%A %e %B %Y %H:%M:%S\n", mktime($time[0], $time[1], $time[2], $months[$str[2]],$str[1],$str[3]));
-	echo strftime("%A %e %B %Y %H:%M:%S", mktime(12,02,21,9,12,2013));
+	$tester = explode(' ', strftime("%A %e %B %Y %H:%M:%S", mktime($time[0], $time[1], $time[2], $months[$str[2]],$str[1],$str[3])));
+	if (preg_match("/^$str[0]/i", $tester[0]) != 1)
+	{
+		echo "Wrong Format\n";
+		return;	
+	}
+	if ($str[1] - $tester[1] != 0)
+	{
+		echo "Wrong Format\n";
+		return;	
+	}
+	if (preg_match("/^$str[2]/i", $tester[2]) != 1)
+	{
+		echo "Wrong Format\n";
+		return;	
+	}
+	if ($str[3] - $tester[3] != 0)
+	{
+		echo "Wrong Format\n";
+		return;	
+	}
+	$result = mktime($time[0], $time[1], $time[2], $months[$str[2]],$str[1],$str[3]) - mktime(0,0,0,1,1,1970);
+	echo "$result\n";
 ?>
