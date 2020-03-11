e234b1de4973336cbf493a34daf970170c63b1fa
Antti Vornanen <avornane@c2r3p2.hive.fi>
2020-03-11 12:35:55 +0200

    Add ssap

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -1,4 +1,15 @@
 #!/usr/bin/php
 <?php
-	echo "Hello"
+	$array = array();
+	unset($argv[0]);
+	foreach($argv as $value)
+	{
+		$arr = array_filter(explode(' ',$value));
+			foreach($arr as $value2)
+				$array[] = $value2;
+	}
+	sort($array);
+	foreach($array as $value){
+		echo "$value\n";
+	}
 ?>
